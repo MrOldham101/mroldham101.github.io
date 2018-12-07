4ae916673d22e5a8485b20e8c493b26e6ce5939e
Kieran Oldham <kieran@reactivestudios.co.uk>
2018-12-07 23:33:09 +0000

    added jumbotron

--- K-Template/scripts.js
+++ K-Template/scripts.js
@@ -1,11 +1,12 @@
 var small = true
-function resizeSB(){
-  if (small == true){
+
+function resizeSB() {
+  if (small == true) {
     document.getElementById("grid-container").style.gridTemplateColumns = "200px auto";
     document.getElementById("resizeButton").innerHTML = '<i class="fas fa-2x fa-arrow-left"></i>';
     document.getElementById("logo").innerHTML = '<b>One</b>Plan';
     small = false;
-  }else{
+  } else {
     document.getElementById("grid-container").style.gridTemplateColumns = "50px auto";
     document.getElementById("resizeButton").innerHTML = '<i class="fas fa-2x fa-arrow-right"></i>';
     document.getElementById("logo").innerHTML = '<b>O</b>P';
