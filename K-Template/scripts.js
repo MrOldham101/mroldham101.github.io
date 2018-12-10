00ce0c896f5380b7d8d3ce51c029e632213d5882
Kieran Oldham <kieran@reactivestudios.co.uk>
2018-12-10 09:31:16 +0000

    added coreui

--- K-Template/scripts.js
+++ K-Template/scripts.js
@@ -2,11 +2,15 @@ var small = true
 
 function resizeSB() {
   if (small == true) {
+    document.getElementById("grid-container").style.animationName = "expandSidebar";
+    document.getElementById("grid-container").style.animationDuration = "0.1s";
     document.getElementById("grid-container").style.gridTemplateColumns = "200px auto";
     document.getElementById("resizeButton").innerHTML = '<i class="fas fa-2x fa-arrow-left"></i>';
     document.getElementById("logo").innerHTML = '<b>One</b>Plan';
     small = false;
   } else {
+    document.getElementById("grid-container").style.animationName = "minimizeSidebar";
+    document.getElementById("grid-container").style.animationDuration = "0.1s";
     document.getElementById("grid-container").style.gridTemplateColumns = "50px auto";
     document.getElementById("resizeButton").innerHTML = '<i class="fas fa-2x fa-arrow-right"></i>';
     document.getElementById("logo").innerHTML = '<b>O</b>P';
